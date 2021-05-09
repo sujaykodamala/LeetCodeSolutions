fa016c16231e4804627b4ec82b3e26a0eacce377
Sujay Kodamala <sujaysiddu@gmail.com>
2021-05-09 23:51:00 +0530

    Time: 588 ms (43.01%), Space: 53.1 MB (37.32%) - LeetHub

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -8,20 +8,19 @@
 
 
 class Solution:
-    def calBranchSums(self, root, depth, depthCollection):
+    def calBranchSums(self, root, depth, minDepth):
         if root is None:
-            depthCollection.append(depth)
-            return depthCollection
-        
+            minDepth = minDepth if depth > minDepth else depth
+            return minDepth
         depth += 1
         if root.left != None and root.right != None:
-            depthCollection = self.calBranchSums(root.left,depth,depthCollection)
-            depthCollection = self.calBranchSums(root.right,depth,depthCollection)
+            minDepth = self.calBranchSums(root.left,depth,minDepth)
+            minDepth = self.calBranchSums(root.right,depth,minDepth)
         elif root.right == None:
-            depthCollection = self.calBranchSums(root.left,depth,depthCollection)
+            minDepth = self.calBranchSums(root.left,depth,minDepth)
         else:
-            depthCollection = self.calBranchSums(root.right, depth, depthCollection)
-        return depthCollection
+            minDepth = self.calBranchSums(root.right, depth, minDepth)
+        return minDepth
         
     def minDepth(self, root: TreeNode) -> int:
         #input: a tree
@@ -29,10 +28,12 @@ class Solution:
         #Approach 1: calculate each depth and return the min depth from the arrays of depths
         #Algorithm1: if root.left != None and root.right != none, calculate depth on both right and left sub tree, 
         #continued-- sort the array and return min value in the array
+        #algorithm2: calculate each depth and compare get the minimum in each calculation
+        #to begin with first value to be compared is infinity. 
         if root is None:
             return 0
-        depth = 0 
-        depthCollection = []
-        depthCollection = self.calBranchSums(root, depth, depthCollection)
-        depthCollection.sort()
-        return 0 if len(depthCollection) ==  0 else depthCollection[0]
+        depth = 0
+        minDepth = float('inf')
+        minDepth = self.calBranchSums(root, depth, minDepth)
+        
+        return minDepth
